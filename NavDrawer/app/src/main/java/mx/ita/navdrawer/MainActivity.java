3693add2164fa7b95e46d179f1ba2dfa1b371fed
VivianaViveroGuzmán <vivianaviveroguzman@gmail.com>
2021-11-09 20:47:46 -0600

    terminsos y condicciones

--- NavDrawer/app/src/main/java/mx/ita/navdrawer/MainActivity.java
+++ NavDrawer/app/src/main/java/mx/ita/navdrawer/MainActivity.java
@@ -39,12 +39,15 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_map2,R.id.nav_listp)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.fragment_nav_terminos,R.id.nav_map2,R.id.nav_listp)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
     }
 
     @Override
