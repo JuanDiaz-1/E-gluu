ba321d915aeeb4bd42cfba89040cbe2bf15b7aae
VivianaViveroGuzmán <vivianaviveroguzman@gmail.com>
2021-12-01 21:18:20 -0600

    correccion how to

--- NavDrawer/app/src/main/java/mx/ita/navdrawer/Howto5.java
+++ NavDrawer/app/src/main/java/mx/ita/navdrawer/Howto5.java
@@ -2,7 +2,9 @@ package mx.ita.navdrawer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Howto5 extends AppCompatActivity {
 
@@ -11,4 +13,19 @@ public class Howto5 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_howto5);
     }
+
+    public void onClick(View view){
+        Intent miIntent=null;
+        Intent intent = new Intent();
+        switch(view.getId()){
+
+            case R.id.imageButtoncancel5:
+                startActivity(new Intent(Howto5.this,MainActivity.class));
+                break;
+
+        }
+
+
+    }
+
 }
